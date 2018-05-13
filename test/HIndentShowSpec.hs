4c9d40cdc113d2e41bdda690174134379e9d190b
Ryo Ota <nwtgck@gmail.com>
2018-05-13 16:39:09 +0900

    [add/test] Implement functions to format and add a test

---
+++ test/HIndentShowSpec.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module HIndentShowSpec where
+
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+
+import qualified Data.String.Here as Here
+import           HIndentShow
+
+main :: IO ()
+main = hspec spec
+
+data Tree a = 
+  Node
+  { value :: a
+  , left  :: (Tree a)
+  , right :: (Tree a)
+  } 
+  | Leaf
+  deriving Show
+
+spec :: Spec
+spec = do
+  describe "hindentShow" $ do
+    it "simple tree" $ do
+      let tree1 =
+            Node
+            { value = 1
+            , left =
+                Node
+                { value = 2
+                , left = Leaf
+                , right =
+                    Node
+                    { value = 1
+                    , left = Node {value = 2, left = Leaf, right = Leaf}
+                    , right = Leaf
+                    }
+                }
+            , right =
+                Node
+                { value = 1
+                , left = Node {value = 2, left = Leaf, right = Leaf}
+                , right = Leaf
+                }
+            }
+      let actual = hindentShow tree1
+      let expect = [Here.here|
+Node
+{ value = 1
+, left =
+    Node
+    { value = 2
+    , left = Leaf
+    , right =
+        Node
+        { value = 1
+        , left = Node {value = 2, left = Leaf, right = Leaf}
+        , right = Leaf
+        }
+    }
+, right =
+    Node
+    { value = 1
+    , left = Node {value = 2, left = Leaf, right = Leaf}
+    , right = Leaf
+    }
+}
+|] ++ "\n"
+      actual `shouldBe` expect
