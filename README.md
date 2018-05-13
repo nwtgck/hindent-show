36336cbc9a3d02608d41e602b99c219da9c693d6
Ryo Ota <nwtgck@gmail.com>
2018-05-13 17:02:40 +0900

    [docs/refactor] Add "Usage" section and refactor test

--- README.md
+++ README.md
@@ -2,3 +2,48 @@
 [![Build Status](https://travis-ci.com/nwtgck/hindent-show.svg?branch=master)](https://travis-ci.com/nwtgck/hindent-show)
 
 Pretty printing by hindent
+
+
+## Usage
+
+Here is an example to pretty-print a tree.
+
+```hs
+data Tree a
+  = Node { value :: a
+         , left :: (Tree a)
+         , right :: (Tree a) }
+  | Leaf
+  deriving (Show)
+
+
+main :: IO
+main = do
+  let tree1 = Node 1 (Node 2 Leaf (Node 1 (Node 2 Leaf Leaf) Leaf)) (Node 1 (Node 2 Leaf Leaf) Leaf)
+  hindentPrint tree1
+```
+
+### output
+
+```txt
+Node
+{ value = 1
+, left =
+    Node
+    { value = 2
+    , left = Leaf
+    , right =
+        Node
+        { value = 1
+        , left = Node {value = 2, left = Leaf, right = Leaf}
+        , right = Leaf
+        }
+    }
+, right =
+    Node
+    { value = 1
+    , left = Node {value = 2, left = Leaf, right = Leaf}
+    , right = Leaf
+    }
+}
+```
--- test/HIndentShowSpec.hs
+++ test/HIndentShowSpec.hs
@@ -12,40 +12,18 @@ import           HIndentShow
 main :: IO ()
 main = hspec spec
 
-data Tree a = 
-  Node
-  { value :: a
-  , left  :: (Tree a)
-  , right :: (Tree a)
-  } 
+data Tree a
+  = Node { value :: a
+         , left :: (Tree a)
+         , right :: (Tree a) }
   | Leaf
-  deriving Show
+  deriving (Show)
 
 spec :: Spec
 spec = do
   describe "hindentShow" $ do
     it "simple tree" $ do
-      let tree1 =
-            Node
-            { value = 1
-            , left =
-                Node
-                { value = 2
-                , left = Leaf
-                , right =
-                    Node
-                    { value = 1
-                    , left = Node {value = 2, left = Leaf, right = Leaf}
-                    , right = Leaf
-                    }
-                }
-            , right =
-                Node
-                { value = 1
-                , left = Node {value = 2, left = Leaf, right = Leaf}
-                , right = Leaf
-                }
-            }
+      let tree1 = Node 1 (Node 2 Leaf (Node 1 (Node 2 Leaf Leaf) Leaf)) (Node 1 (Node 2 Leaf Leaf) Leaf)
       let actual = hindentShow tree1
       let expect = [Here.here|
 Node
